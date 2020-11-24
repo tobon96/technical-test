f8fb8af8c20cf3eee96625a5ffbb64b49fb13ace
Juan Pablo Tobon Florez <juan.tobon@experimentality.co>
2020-11-24 00:08:35 -0500

    OpenApi api docs

--- src/main/java/com/wolox/technicalTest/constants/UtilConstants.java
+++ src/main/java/com/wolox/technicalTest/constants/UtilConstants.java
@@ -4,7 +4,6 @@ public final class UtilConstants {
 
     ///// JSONPLACEHOLDER URL
     public static final String API_URL = "https://jsonplaceholder.typicode.com";
-
     public static final String COMMENTS_BASE = "/comments";
     public static final String COMMENTS_EMAIL = "/comments?email=";
     public static final String COMMENTS_NAME = "/comments?name=";
